632f627dada3eb4150961f0c13ccff09057bed5d
matthias mitterreiter <matthias@mitterreiter.de>
2019-02-20 18:08:43 +0100

    done

--- particles.cc
+++ particles.cc
@@ -8,71 +8,84 @@
 // http://fabiensanglard.net/doom_fire_psx/index.html
 
 const std::vector<sf::Color> cv{
-    sf::Color(0x07, 0x07, 0x07), sf::Color(0x1F, 0x07, 0x07),
-    sf::Color(0x2F, 0x0F, 0x07), sf::Color(0x47, 0x0F, 0x07),
-    sf::Color(0x57, 0x17, 0x07), sf::Color(0x67, 0x1F, 0x07),
-    sf::Color(0x77, 0x1F, 0x07), sf::Color(0x8F, 0x27, 0x07),
-    sf::Color(0x9F, 0x2F, 0x07), sf::Color(0xAF, 0x3F, 0x07),
-    sf::Color(0xBF, 0x47, 0x07), sf::Color(0xC7, 0x47, 0x07),
-    sf::Color(0xDF, 0x4F, 0x07), sf::Color(0xDF, 0x57, 0x07),
-    sf::Color(0xDF, 0x57, 0x07), sf::Color(0xD7, 0x5F, 0x07),
-    sf::Color(0xD7, 0x5F, 0x07), sf::Color(0xD7, 0x67, 0x0F),
-    sf::Color(0xCF, 0x6F, 0x0F), sf::Color(0xCF, 0x77, 0x0F),
-    sf::Color(0xCF, 0x7F, 0x0F), sf::Color(0xCF, 0x87, 0x17),
-    sf::Color(0xC7, 0x87, 0x17), sf::Color(0xC7, 0x8F, 0x17),
-    sf::Color(0xC7, 0x97, 0x1F), sf::Color(0xBF, 0x9F, 0x1F),
-    sf::Color(0xBF, 0x9F, 0x1F), sf::Color(0xBF, 0xA7, 0x27),
-    sf::Color(0xBF, 0xA7, 0x27), sf::Color(0xBF, 0xAF, 0x2F),
-    sf::Color(0xB7, 0xAF, 0x2F), sf::Color(0xB7, 0xB7, 0x2F),
-    sf::Color(0xB7, 0xB7, 0x37), sf::Color(0xCF, 0xCF, 0x6F),
-    sf::Color(0xDF, 0xDF, 0x9F), sf::Color(0xEF, 0xEF, 0xC7),
-    sf::Color(0xFF, 0xFF, 0xFF)};
+    sf::Color(0xFF, 0xFF, 0xFF), sf::Color(0xEF, 0xEF, 0xC7),
+    sf::Color(0xDF, 0xDF, 0x9F), sf::Color(0xCF, 0xCF, 0x6F),
+    sf::Color(0xB7, 0xB7, 0x37), sf::Color(0xB7, 0xB7, 0x2F),
+    sf::Color(0xB7, 0xAF, 0x2F), sf::Color(0xBF, 0xAF, 0x2F),
+    sf::Color(0xBF, 0xA7, 0x27), sf::Color(0xBF, 0xA7, 0x27),
+    sf::Color(0xBF, 0x9F, 0x1F), sf::Color(0xBF, 0x9F, 0x1F),
+    sf::Color(0xC7, 0x97, 0x1F), sf::Color(0xC7, 0x8F, 0x17),
+    sf::Color(0xC7, 0x87, 0x17), sf::Color(0xCF, 0x87, 0x17),
+    sf::Color(0xCF, 0x7F, 0x0F), sf::Color(0xCF, 0x77, 0x0F),
+    sf::Color(0xCF, 0x6F, 0x0F), sf::Color(0xD7, 0x67, 0x0F),
+    sf::Color(0xD7, 0x5F, 0x07), sf::Color(0xD7, 0x5F, 0x07),
+    sf::Color(0xDF, 0x57, 0x07), sf::Color(0xDF, 0x57, 0x07),
+    sf::Color(0xDF, 0x4F, 0x07), sf::Color(0xC7, 0x47, 0x07),
+    sf::Color(0xBF, 0x47, 0x07), sf::Color(0xAF, 0x3F, 0x07),
+    sf::Color(0x9F, 0x2F, 0x07), sf::Color(0x8F, 0x27, 0x07),
+    sf::Color(0x77, 0x1F, 0x07), sf::Color(0x67, 0x1F, 0x07),
+    sf::Color(0x57, 0x17, 0x07), sf::Color(0x47, 0x0F, 0x07),
+    sf::Color(0x2F, 0x0F, 0x07), sf::Color(0x1F, 0x07, 0x07),
+    sf::Color(0x07, 0x07, 0x07)};
 
 std::unordered_map<unsigned int, int> um{
-    {117901311, 0},   {520554495, 1},   {789514239, 2},   {1192167423, 3},
-    {1461127167, 4},  {1730086911, 5},  {1998522367, 6},  {2401699839, 7},
-    {2670659583, 8},  {2940143615, 9},  {3209103359, 10}, {3343321087, 11},
-    {3746498559, 12}, {3747022847, 13}, {3747022847, 14}, {3613329407, 15},
-    {3613329407, 16}, {3613855743, 17}, {3480162303, 18}, {3480686591, 19},
-    {3481210879, 20}, {3481737215, 21}, {3347519487, 22}, {3348043775, 23},
-    {3348570111, 24}, {3214876671, 25}, {3214876671, 26}, {3215403007, 27},
-    {3215403007, 28}, {3215929343, 29}, {3081711615, 30}, {3082235903, 31},
-    {3082237951, 32}, {3486478335, 33}, {3755974655, 34}, {4025468927, 35},
-    {4294967295, 36}};
+    {117901311, 36},  {520554495, 35},  {789514239, 34},  {1192167423, 33},
+    {1461127167, 32}, {1730086911, 31}, {1998522367, 30}, {2401699839, 29},
+    {2670659583, 28}, {2940143615, 27}, {3209103359, 26}, {3343321087, 25},
+    {3746498559, 24}, {3747022847, 23}, {3747022847, 22}, {3613329407, 21},
+    {3613329407, 20}, {3613855743, 19}, {3480162303, 18}, {3480686591, 17},
+    {3481210879, 16}, {3481737215, 15}, {3347519487, 14}, {3348043775, 13},
+    {3348570111, 12}, {3214876671, 11}, {3214876671, 10}, {3215403007, 9},
+    {3215403007, 8},  {3215929343, 7},  {3081711615, 6},  {3082235903, 5},
+    {3082237951, 4},  {3486478335, 3},  {3755974655, 2},  {4025468927, 1},
+    {4294967295, 0}};
+
+// void doFire(std::vector<sf::Vertex> &pixMat, int W, int H) {
+//   for (int x = 0; x < W; ++x) {
+//     for (int y = H - 1; y >= 1; --y) {
+//       int dest = y * W + x;
+//       int src = (dest - (rand() % 2) * W) - (rand() % 3) + 1;
+//       int color_index = um[pixMat[src].color.toInteger()];
+
+//       color_index = color_index - (rand() & 1);
+//       color_index = std::max(color_index, 0);
+//       pixMat[dest].color = cv[color_index];
+//     }
+//   }
+// }
 
 void doFire(std::vector<sf::Vertex> &pixMat, int W, int H) {
   for (int x = 0; x < W; ++x) {
-    for (int y = H - 1; y >= 1; --y) {
+    for (int y = cv.size() * 2; y >= 1; --y) {
       int dest = y * W + x;
       int src = (dest - (rand() % 2) * W) - (rand() % 3) + 1;
-
       int color_index = um[pixMat[src].color.toInteger()];
-      color_index = color_index - (rand() & 1);
-      color_index = std::max(color_index, 0);
+
+      color_index = color_index + (rand() & 1);
+      color_index = std::min(color_index, ((int)cv.size()) - 1);
       pixMat[dest].color = cv[color_index];
     }
   }
 }
 int main() {
   const int H = 400;
-  const int W = 500;
+  const int W = 1200;
 
   sf::RenderTexture RT;
   RT.create(W, H);
 
   sf::RenderWindow window(sf::VideoMode(W, H, 32), "Test");
-  window.setFramerateLimit(30);
+  window.setFramerateLimit(60);
 
-  // Creates the sf::Vertex matrix
   std::vector<sf::Vertex> pixMat(H * W);
 
   for (int x = 0; x < W; ++x)
     for (int y = 0; y < H; ++y) {
-      sf::Color c = (y == 0) ? cv[cv.size() - 1] : cv[0];
+      sf::Color c = (y == 0) ? cv[0] : cv[cv.size() - 1];
       pixMat[(y * W + x)] = sf::Vertex(sf::Vector2f(x, y), c);
     }
 
-  int counter = 100;
+  int counter = 1000;
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
